f0f0a16d9ae5141a32d9c17cc3c7d60aaa783598
Parthiv070 <parthivchudasama5@gmail.com>
2025-09-27 09:44:40 +0530

    Pushing Quiz Application

---
+++ src/main/java/com/quiz/QuizApplication/Controller/QuizController.java
@@ -0,0 +1,36 @@
+package com.quiz.QuizApplication.Controller;
+
+import com.quiz.QuizApplication.Entity.Response;
+import com.quiz.QuizApplication.Entity.QuestionWrapper;
+import com.quiz.QuizApplication.Service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    private QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category,
+                                             @RequestParam int numQ,
+                                             @RequestParam String title) {
+        return quizService.createquiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(@PathVariable int id) {
+        return quizService.getQuizQuestion(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable int id,
+                                              @RequestBody List<Response> responses) {
+        return quizService.calculateResult(id, responses);
+    }
+}
